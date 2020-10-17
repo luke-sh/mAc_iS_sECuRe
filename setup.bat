bfe6d9f656ffb63f026bb041d27ac0655822ffd7
luke-sh <73024753+luke-sh@users.noreply.github.com>
2020-10-17 14:41:38 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,3 @@
+python -m pip install colorama
+go get github.com/denisbrodbeck/machineid
+go get github.com/reujab/wallpaper
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python3 -m pip install colorama
+go get github.com/denisbrodbeck/machineid;
+go get github.com/reujab/wallpaper;
